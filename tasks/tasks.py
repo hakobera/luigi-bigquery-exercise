183edce16cd63ae59f57f1fa65fb962a128c179b
Kazuyuki Honda <hakobera@gmail.com>
2015-11-19 10:55:03 +0900

    Add pandas example

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -2,7 +2,9 @@
 
 import luigi
 import luigi_bigquery
+
 from datetime import datetime as dt
+import six
 
 class DailyTask(luigi.Task):
     day = luigi.DateParameter()
@@ -38,7 +40,6 @@ class BatchTask(luigi.Task):
 
 class DailyTop10Account(luigi_bigquery.QueryTable):
     day = luigi.DateParameter()
-
     source = "queries/daily_top10_account.sql"
 
     def dataset(self):
@@ -46,6 +47,20 @@ class DailyTop10Account(luigi_bigquery.QueryTable):
 
     def table(self):
         return "{0}_{1}".format('daily_top10_acount', self.day.strftime('%Y%m%d'))
+
+class PandasExample(luigi_bigquery.Query):
+    day = luigi.DateParameter()
+    source = "queries/daily_top10_account.sql"
+
+    def output(self):
+        return luigi.LocalTarget("output/PandasExample-{0}.txt".format(self.day.strftime('%Y%m%d')))
+
+    def run(self):
+        query = self.load_query(self.source)
+        result = self.run_query(query)
+        with self.output().open('w') as f:
+            f.write(result.to_dataframe().to_string())
+
 
 if __name__ == '__main__':
     import sys
